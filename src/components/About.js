57cb91885d75f949d28f3cc6c7ef8dfb069d6e9e
PiyushPagar <piyushpagar79@gmail.com>
2022-01-25 15:31:16 +0530

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <h3>Version 1.6.2001</h3>
+            <a href="/">Go Back</a>
+        </div>
+    )
+}
+
+export default About
